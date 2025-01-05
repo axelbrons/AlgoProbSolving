2f2e269b706308d1266e5309a45a7435e41cac04
Axel Bröns <116881950+axelbrons@users.noreply.github.com>
2025-01-05 19:02:05 +0100

    add c solutions files

---
+++ c/power.c
@@ -0,0 +1,100 @@
+// powerFunction.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+double myPow(double x, int n) {
+    if (n > 0) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * myPow(x, n - 1);
+    }
+    else {
+        if (n == 0) {
+            return 1;
+        }
+        return (1/x) * (myPow(x, n + 1));
+    }
+}
+
+double myPow2(double x, int n) {
+    double res = 1;
+    if (x == 1) {
+        return x;
+    }
+    if (x == -1 && n % 2 == 0) {
+        return -x;
+    }
+    if (x == -1 && n % 2 == 1) {
+        return x;
+    }
+
+    if (n == -2147483648) {
+        n += 1;
+    }
+    for (int i = 0; i < abs(n); i++) {
+        res *= x;
+        if (n>0 && res > 10001) {
+            break;
+        }
+        if (n < 0 && res > 100001) {
+            break;
+        }
+    }
+    if (n > 0) {
+        return res;
+    }
+    else {
+        return 1 / res;
+    }
+    pow(2, 5);
+    
+}
+
+//Solution optimale
+double myPow5(double x, int n) {
+    long long min = -2147483648, max = 2147483647;
+    if (min <= n && n <= max) {
+        if (x > -100.0 && x < 100.0) {
+            if (n < 0) {
+                if (n == INT_MIN) {
+                    n = INT_MAX;
+                    x = 1 / (x * x);
+                }
+                else {
+                    x = 1 / x;
+                    n = -n;
+                }
+            }
+            double res = 1;
+            while (n > 0) {
+                if (n & 1) {		
+
+                    res *= x; 
+                }
+                x *= x;
+                n >>= 1;
+            }
+            return res;
+        }
+        else {
+            return 0;
+        }
+    }
+    else {
+        return 0;
+    }
+}
+
+
+
+
+
+
+int main()
+{
+    double result;
+    result = myPow5(2,7);
+    printf("%f",result);
+}
---
+++ c/remove_element.c
@@ -0,0 +1,50 @@
+// Remove Element.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int comp(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+int removeElement(int* nums, int numsSize, int val) {
+
+    int k = 0;
+
+    if (val > 50) {
+        return numsSize;
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        if (val == nums[i]) {
+            nums[i] = 101;
+            k++;
+        }
+    }
+    qsort(nums, numsSize, sizeof(int), comp);
+    /*
+    for (int i = 0; i < numsSize; i++) {
+        printf("%d ,", nums[i]);
+    }
+    */
+    
+    return numsSize - k;
+    
+}
+
+
+
+
+
+int main()
+{
+    const int numsSize = 4;
+    int nums[numsSize] = { 3,2,2,3 };
+    int val = 4;
+    int k;
+    k = removeElement(nums, numsSize, val);
+    printf("%d", k);
+
+}
+
+
